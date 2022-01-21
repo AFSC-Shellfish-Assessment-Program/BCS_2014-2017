e4d83ccc65062245e64e0bc50688ffd73aae4949
mikelitzow <mike.litzow@noaa.gov>
2022-01-20 17:38:16 -0900

    Adding exploratory opilio plots

--- scripts/explore_oplio.R
+++ scripts/explore_oplio.R
@@ -1,183 +1,128 @@
-# explore and analyze infection dynamics in C. opilio
+# explore and analyze infection dynamics in C. bairdi
 
 library(tidyverse)
-
-# load PCR data for each year
-d1=read.csv("./data/bcs14.csv")
-d2=read.csv("./data/bcs15.csv")
-d3=read.csv("./data/bcs16.csv") 
-
-names(d1); names(d2); names(d3)
-
-name.check <- data.frame(names1 = names(d1),
-                         names2 = names(d2),
-                         names3 = names(d3))
-
-name.check # so names are interchangeable, just some formatting differences
-
-names(d2) <- names(d3) <- names(d1)
-dat = rbind(d1, d2, d3) 
-
-str(dat)
-
-# set up an index column
-substr(dat$Specific_Location,10,10)
-dat$index = as.numeric(substr(dat$Specific_Location,10,10))
-
-#it appears there are some crabs from outside index areas?
-unique(dat$index)
-idxs = is.na(dat$index)
-sum(idxs) # only one!
-which(idxs == T)
-dat = dat[!idxs,]
-
-dat = dat[dat$PCR_result == 0 | dat$PCR_result == 1,] # limiting to PCR positive or negative - dropping code 3 (results uncertain)
-
-unique(dat$Species_Name)
-
-# removing species == NA
-idxs = dat$Species_Name == ""
-sum(idxs) # only one
-
-dat=dat[!idxs,]
+library(plyr)
+library(rstan)
+library(brms)
+library(bayesplot)
+source("./scripts/stan_utils.R")
 
 
-# check clutch codes 
-check <- dat %>%
-  group_by(Species_Name, Clutch) %>%
-  summarise(count = n())
+# set plot theme
+theme_set(theme_bw())
 
-check # that's a lot of data to throw away!
+# colorblind palette
+cb <- c("#999999", "#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7") 
 
 
-# select opilio for analysis
+# load PCR data 
+dat <- read.csv("./data/pcr_haul_master.csv")
 
-opilio <- dat %>%
-  filter(Species_Name == "Chionoecetes opilio",
-         index %in% c(4, 5, 6))
+# add julian date
+dat$julian = lubridate::yday(lubridate::parse_date_time(x = dat$start_date, orders="mdy", tz="US/Alaska"))                                
 
-opilio$index <- as.factor(opilio$index)
+# examine Snow crab cpue distribution                                    
+ggplot(dat, aes(snow70under_cpue)) +
+  geom_histogram(bins = 30, fill = "grey", color = "black")
+
+ggplot(dat, aes(snow70under_cpue^0.25)) +
+  geom_histogram(bins = 30, fill = "grey", color = "black")
+
+# separate Tanner data
+opilio.dat <- dat %>%
+  dplyr::filter(species_name == "Chionoecetes opilio",
+                index_site %in% c(4, 5, 6),
+                year %in% c(2015:2017),
+                sex %in% c(1, 2),
+                pcr_result %in% c(1, 0)) %>%
+  dplyr::select(pcr_result, size, sex, index_site, year, gis_station, julian, mid_longitude, bottom_depth, gear_temperature, snow70under_cpue) %>%
+  dplyr::rename(pcr = pcr_result,
+                station = gis_station,
+                longitude = mid_longitude,
+                depth = bottom_depth,
+                temperature = gear_temperature,
+                index = index_site,
+                fourth.root.cpue70 = snow70under_cpue) %>%
+  dplyr::mutate(year = as.factor(year),
+                sex = as.factor(sex),
+                index = as.factor(index),
+                station = as.factor(station),
+                fourth.root.cpue70 = fourth.root.cpue70^0.25) # transforming cpue here
+
+nrow(opilio.dat) # 1511 samples!
 
 # examine sample size by index / year
 
 # now the same for index/year
-plot <- opilio %>%
-  dplyr::select(Year, index, PCR_result) %>%
-  dplyr::group_by(Year, index) %>%
-  dplyr::summarise(PCR_0 = sum(PCR_result == 0),
-                   PCR_1 = sum(PCR_result == 1)) %>%
-  tidyr::pivot_longer(cols = c(-Year, -index))
+plot <- opilio.dat %>%
+  dplyr::select(year, index, pcr) %>%
+  dplyr::group_by(year, index) %>%
+  dplyr::summarise(PCR_0 = sum(pcr == 0),
+                   PCR_1 = sum(pcr == 1)) %>%
+  tidyr::pivot_longer(cols = c(-year, -index))
 
 ggplot(plot, aes(name, value)) +
   geom_bar(stat = "identity") +
-  facet_grid(Year ~ index)
+  facet_grid(year ~ index)
 
 ggsave("./figs/opilio_index_year_incidence.png", width = 6, height = 6, units = "in")
 
-# plot surface temp - bottom temp relationship
-ggplot(opilio, aes(SURFACE_TEMP, Bottom_Temp)) +
-  geom_point() +
-  geom_smooth(method = "gam", formula = y ~ s(x, k = 3), se = F) +
-  geom_point(aes(SURFACE_TEMP, Bottom_Temp, color = index))
 
-# very different from Tanner!
-ggsave("./figs/opilio_surface_temp_bottom_temp.png", width = 6, height = 4, units = "in")
-
-# and plot surface / bottom temps / bottom depth / dates by year / index
-
-# first, calculate julian day
-opilio$date <- stringr::str_split_fixed(opilio$START_TIME, " ", n = 2)[,1] # remove time, keep date
-
-opilio$julian <- lubridate::yday(chron::dates(opilio$date))
-
-plot <- opilio %>%
-  dplyr::group_by(Year, index, STATIONID) %>%
-  dplyr::summarise(surface = mean(SURFACE_TEMP),
-                   bottom = mean(Bottom_Temp),
-                   depth = mean(BOTTOM_DEPTH),
+plot <- opilio.dat %>%
+  dplyr::group_by(year, index, station) %>%
+  dplyr::summarise(temperature = mean(temperature),
+                   depth = mean(depth),
                    julian = mean(julian)) 
 
-
-ggplot(plot, aes(bottom)) +
+ggplot(plot, aes(temperature)) +
   geom_histogram(bins = 12, fill = "dark grey", color = "black") +
-  facet_grid(Year ~ index)
+  facet_grid(year ~ index)
 
 ggsave("./figs/opilio_bottom_temp_by_index.png", width = 5, height = 4, units = "in")
 
 ggplot(plot, aes(depth)) +
   geom_histogram(bins = 12, fill = "dark grey", color = "black") +
-  facet_grid(Year ~ index)
+  facet_grid(year ~ index)
 
 ggsave("./figs/opilio_depth_by_index.png", width = 5, height = 4, units = "in")
 
 ggplot(plot, aes(julian)) +
   geom_histogram(bins = 12, fill = "dark grey", color = "black") +
-  facet_grid(Year ~ index) # big differences among index areas
+  facet_grid(year ~ index) # big differences among index areas
 
 ggsave("./figs/opilio_date_by_index.png", width = 5, height = 4, units = "in")
 
-ggplot(plot, aes(julian, bottom)) +
-  geom_point() # this is a big problem - julian day is collinear with temperature
+ggplot(plot, aes(julian, temperature)) +
+  geom_point() # julian day is no longer collinear with temperature once 2017 is included
 
 ggsave("./figs/opilio_date_vs_bottom_temp.png", width = 6, height = 4, units = "in")
 
 
 # look at Julian day as a predictor of % positive
-plot <- opilio %>%
-  dplyr::group_by(Year, STATIONID) %>%
+plot <- opilio.dat %>%
+  dplyr::group_by(year, station) %>%
   dplyr::summarise(julian = mean(julian), 
                    n = dplyr::n(),
-                   proportion.positive = sum(PCR_result == 1) / dplyr::n())
+                   proportion.positive = sum(pcr == 1) / dplyr::n())
 
 ggplot(plot, aes(julian, proportion.positive)) +
   geom_point() + 
-  facet_wrap(~Year) +
-  geom_smooth(method = "gam")
+  facet_wrap(~year) +
+  geom_smooth(method = "gam", se = F)
 
 ggsave("./figs/opilio_date_vs_percent_positive.png", width = 6, height = 4, units = "in")
 
 
 # and size as a predictor of % positive
-plot <- opilio %>%
-  dplyr::group_by(Year, STATIONID) %>%
-  dplyr::summarise(size = mean(Size), 
+plot <- opilio.dat %>%
+  dplyr::group_by(year, station) %>%
+  dplyr::summarise(size = mean(size), 
                    n = dplyr::n(),
-                   proportion.positive = sum(PCR_result == 1) / dplyr::n())
+                   proportion.positive = sum(pcr == 1) / dplyr::n())
 
 ggplot(plot, aes(size, proportion.positive)) +
   geom_point() + 
-  facet_wrap(~Year) +
+  facet_wrap(~year) +
   geom_smooth(method = "gam")
 
 ggsave("./figs/opilio_size_vs_percent_positive.png", width = 6, height = 4, units = "in")
-
-# and size distribution by index / year
-
-ggplot(opilio, aes(Size)) +
-  geom_histogram(bins = 30, fill = "grey", color = "black") +
-  facet_grid(Year ~ index)
-
-
-plot <- opilio %>%
-  dplyr::group_by(Year, index) %>%
-  dplyr::summarise(size = mean(Size), 
-                   n = dplyr::n(),
-                   proportion.positive = sum(PCR_result == 1) / dplyr::n())
-
-
-# make a toy model with julian day, size, and year as predictors
-
-toy.dat <- opilio %>%
-  dplyr::group_by(Year, STATIONID) %>%
-  dplyr::summarise(size = mean(Size),
-                   julian = mean(julian),
-                   proportion.positive = sum(PCR_result == 1) / dplyr::n())
-
-
-toy.dat$Year <- as.factor(toy.dat$Year)
-
-
-mod <- mgcv::gam(proportion.positive ~ Year + s(julian, k = 4) + s(size, k = 4), dat = toy.dat)
-summary(mod)
-
-plot(mod)
