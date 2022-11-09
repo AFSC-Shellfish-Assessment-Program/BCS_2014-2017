9c5eb70c51889b3761f0c6f71920b5730cfc62fd
Erin-Fedewa-NOAA <Erin.Fedewa@nmfs.local>
2022-11-09 12:00:44 -0900

    Adding bayes latent-class model exploration

--- scripts/sensitivity_analysis.R
+++ scripts/sensitivity_analysis.R
@@ -32,6 +32,15 @@ dat %>%
 #Wow...7% probability of diagnosing visually + when hematodinium is present 
   #99% probability of diagnosing visually neg when hemato is absent 
 
+#Calculate sample size and binomial CI's
+a <- .05 #significance level
+dat2 %>%
+  group_by(species_name, index, year) %>%
+  summarise(n = n()) %>%
+  full_join(prev) %>%
+  mutate(CI_upper = Prevalance + (qnorm(1-a/2))*sqrt((1/n)*Prevalance*(100-Prevalance)),
+         CI_lower = Prevalance + (-qnorm(1-a/2))*sqrt((1/n)*Prevalance*(100-Prevalance))) -> prev_n
+
 ###############################################
 #Does specificity/sensitivity differ by crab size/seasonality? May need to follow up
 #on this approach, but for now, let's assess variation in sensitivity in subgroups of 
@@ -98,3 +107,47 @@ dat2 %>%
 
 #Sample size of visually positive crab is so small that we really don't see much 
   #difference in sensitivity by date sampled or crab size 
+
+######################################################
+#Exploration of a bayesian latent-class model for sensitivity & specificity
+  #in the absence of a gold standard 
+
+#Approach 1: JAGS model
+library(readxl)
+library(runjags)
+library(rjags)
+testjags()
+
+Sys.setenv(JAGS_HOME = "C:/Users/erin.fedewa/AppData/Local/Programs/JAGS/JAGS-4.3.1/x64/bin/jags-terminal.exe")
+ 
+#Reference: https://github.com/paoloeusebi/BLCM-Covid19/blob/master/covid_r1_ind_I.R
+#https://academic.oup.com/aje/article/190/8/1689/6206818
+
+#Model structure for run.jags() is a little over my head....
+
+#Approach 2: R Shiny App for latent class models
+#https://www.nandinidendukuri.com/how-to-guide-on-a-r-shiny-app-to-do-bayesian-inference-for-diagnostic-meta-analysis/
+dat2 %>%
+  filter(julian <= 198) %>%
+  summarise(TP_n = nrow(filter(.,sen_spec=="TP")),
+            FP_n = nrow(filter(.,sen_spec=="FP")),
+            FN_n = nrow(filter(.,sen_spec=="FN")),
+            TN_n = nrow(filter(.,sen_spec=="TN")))
+
+dat2 %>%
+  filter(julian > 198) %>%
+  summarise(TP_n = nrow(filter(.,sen_spec=="TP")),
+            FP_n = nrow(filter(.,sen_spec=="FP")),
+            FN_n = nrow(filter(.,sen_spec=="FN")),
+            TN_n = nrow(filter(.,sen_spec=="TN")))
+
+tp <- c(20,36) 
+fp <- c(1,1) 
+fn <- c(242,477) 
+tn <- c(1316,715) 
+cell <- cbind(tp, fp, fn, tn)
+n <- length(tp)  #  Number of studies      
+write("tp fp fn tn","output/Sens_Data.txt")
+for (j in 1:n) write(cell[j,],"output/Sens_Data.txt",append=T)
+
+#Output used to run model on R shiny app 
